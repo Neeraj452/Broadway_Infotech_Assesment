7214667418826084dfac6aace21495f3d0778843
neeraj.maurya <neeraj.maurya@innovationm.com>
2024-04-11 12:25:42 +0530

    fixed the sidebar issue

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -40,32 +40,32 @@ const Sidebar = () => {
             <div className="position-absolute menubar_parent px-3">
                 <i className="menubar ri-menu-line ri-lg text-dark" onClick={() => setActive(!active)}></i>
             </div>
-            <div className='px-4'>
+            <div className={`${active?' d-flex justify-content-center':'px-4'}`}>
                 <img src={!active ? marketme : rocket} alt="" width={active ? 35 : 190} height={40} class="px-1"/>
             </div>
             <div className="mt-4 p-0">
                 <ul type="none" className="list-group gap-1">
-                    <li className=" d-flex gap-3 align-items-center py-2 px-4 rounded-2">
+                    <li className={`d-flex gap-3 align-items-center py-2 ${active?'justify-content-center':'px-4'} rounded-2`}>
                         <i className="ri-dashboard-line ri-lg py-1"></i>
                         <span className={`${active ? "d-none" : ""} flex-shrink-0`}>Dashboard</span>
                     </li>
-                    <li className=" d-flex gap-3 py-2 px-4 rounded-2 align-items-center">
+                    <li className={`d-flex gap-3 py-2 ${active?'justify-content-center':'px-4'} rounded-2 align-items-center`}>
                         <div>
                             <img src={strategy} alt="" />
                         </div>
                         <span className={`${active ? "d-none" : ""} flex-shrink-0`}>Strategy</span>
                     </li>
                     {data.map((item, index) => {
-                        return <li className={`d-flex gap-3 py-2 px-4 rounded-2 align-items-center ${item.name==="Content Creation"? 'selected':null}`}>
+                        return <li className={`d-flex gap-3 py-2 ${active?'justify-content-center':'px-4'} rounded-2 align-items-center ${item.name==="Content Creation"? 'selected':null}`}>
                             <i className={`${item.icon} ri-lg py-1`}></i>
                             <span className={`${active ? "d-none" : ""} flex-shrink-0`}>{item.name}</span>
                         </li>
                     })}
                 </ul>
-                <h6 className={`my-3 help px-4`}>Help</h6>
+                <h6 className={`my-3 help ${active?'px-3':'px-4'}`}>Help</h6>
                 <ul type="none" className=" list-group">
                     {data1.map((item, index) => {
-                        return <li className=" d-flex gap-3 py-2 px-4 rounded-2 align-items-center">
+                        return <li className={`d-flex gap-3 py-2 ${active?'justify-content-center':'px-4'} rounded-2 align-items-center`}>
                             <i className={`${item.icon} ri-lg py-1`}></i>
                             <span className={`${active ? "d-none" : ""} flex-shrink-0`}>{item.name}</span>
                         </li>
